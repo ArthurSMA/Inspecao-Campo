1f160507bbe7c2c95f29710a3a1a2ad5520170f8
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2026-08-07 13:06:52 -0300

    feat(database): configurando drift e sqlite

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/app/app.dart';
+import 'package:flutter_application_1/core/database/app_database.dart';
 
 void main() {
   runApp(const App());
 }
+
+final database = AppDatabase();
